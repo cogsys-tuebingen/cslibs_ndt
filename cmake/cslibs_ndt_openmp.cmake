0872bc80c65d3e0fc738d5d306de5b2114b67c39
Richard Hanten <richard.hanten@uni-tuebingen.de>
2017-11-08 18:08:48 +0100

    starting over this package

---
+++ cmake/cslibs_ndt_openmp.cmake
@@ -0,0 +1,7 @@
+if(${MUSE_SMC_OPEN_MP})
+    find_package(OpenMP  REQUIRED)
+    set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
+    message("[${PROJECT_NAME}]: Compilining with OpenMP!")
+endif()

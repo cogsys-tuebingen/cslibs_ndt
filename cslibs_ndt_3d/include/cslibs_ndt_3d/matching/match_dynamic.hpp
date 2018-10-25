540f8385408531d99c418050d08e7373567a0d9b
Daejiv <daejiv@googlemail.com>
2018-10-25 12:46:15 +0200

    matching: migrate to new interface part 1

--- cslibs_ndt_3d/include/cslibs_ndt_3d/matching/match_dynamic.hpp
+++ cslibs_ndt_3d/include/cslibs_ndt_3d/matching/match_dynamic.hpp
@@ -1,7 +1,7 @@
 #ifndef CSLIBS_NDT_3D_MATCH_DYNAMIC_HPP
 #define CSLIBS_NDT_3D_MATCH_DYNAMIC_HPP
 
-#include <cslibs_ndt_3d/matching/match.hpp>
+#include <cslibs_ndt/matching/match.hpp>
 #include <cslibs_ndt_3d/dynamic_maps/gridmap.hpp>
 
 #include <cslibs_ndt_3d/matching/voxel.hpp>
@@ -11,25 +11,15 @@ namespace matching {
 namespace dynamic_maps {
 inline void match(const cslibs_math_3d::Pointcloud3d::ConstPtr &src,
                   const cslibs_math_3d::Pointcloud3d::ConstPtr &dst,
-                  const Parameters                             &params,
-                  Result                                       &r)
+                  const cslibs_ndt::matching::Parameter        &params,
+                  double                                        resolution,
+                  const cslibs_math_3d::Transform3d            &initial_transform,
+                  cslibs_ndt::matching::Result<cslibs_math_3d::Transform3d> &r)
 {
   using ndt_t = cslibs_ndt_3d::dynamic_maps::Gridmap;
-  ndt_t::Ptr ndt(new ndt_t(ndt_t::pose_t(), params.resolution()));
-  ndt->insert(dst);
-  impl::match<ndt_t>(src, ndt, params, r);
-}
-
-template<std::size_t Ts>
-inline void match(const cslibs_math_3d::Pointcloud3d::ConstPtr &src,
-                  const cslibs_math_3d::Pointcloud3d::ConstPtr &dst,
-                  const Parameters                             &params,
-                  Result                                       &r)
-{
-  using ndt_t = cslibs_ndt_3d::dynamic_maps::Gridmap;
-  ndt_t::Ptr ndt(new ndt_t(ndt_t::pose_t(), params.resolution()));
-  ndt->insert(dst);
-  impl::match<ndt_t, Ts>(src, ndt, params, r);
+  ndt_t ndt(ndt_t::pose_t(), resolution);
+  ndt.insert(dst);
+  cslibs_ndt::matching::match(src->begin(), src->end(), ndt, params, initial_transform, r);
 }
 }
 }

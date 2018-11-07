6b064e6f03019a24f6544b5e72b1a2b793acb029
Richard Hanten <richard.hanten@uni-tuebingen.de>
2018-11-07 15:27:20 +0100

    sympy

---
+++ cslibs_ndt_2d/res/ndt_sym_2d.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from sympy import *
+
+tx,ty,gamma = symbols('tx ty gamma')
+x,y = symbols('x y')
+
+R_z = Matrix([[cos(gamma),-sin(gamma)],[sin(gamma), cos(gamma)]])
+
+v = Matrix([[x],[y]])
+t = Matrix([[tx],[ty]])
+
+v_prime = (R_z * v) + t
+
+d = Matrix([gamma])
+J = v_prime.jacobian(d)
+
+init_printing()
+
+pprint(v_prime)
+pprint(J)
+
+print(latex(v_prime[0]))
+print("-------------------------")
+print(latex(v_prime[1]))
+print("-------------------------")
+print(latex(J.col(0)))
+print("-------------------------")
+print(latex(J.jacobian(d)))
+

8f9ae04734b629c13b6ef58d80d3281d1594cbed
Cornelia Schulz <cornelia.schulz@uni-tuebingen.de>
2019-03-12 17:19:36 +0100

    changed to call by ref

--- cslibs_ndt/include/cslibs_ndt/utility/binary_indices.hpp
+++ cslibs_ndt/include/cslibs_ndt/utility/binary_indices.hpp
@@ -16,7 +16,7 @@ inline constexpr std::size_t two_pow(const std::size_t Dim)
 
 
 template <typename index_t>
-inline constexpr int get_index(const index_t bi, const std::size_t counter, const std::size_t dimension)
+inline constexpr int get_index(const index_t &bi, const std::size_t counter, const std::size_t dimension)
 {
     return ((counter / two_pow(dimension)) % 2 == 0) ?
               cslibs_math::common::div(bi[dimension],2) :
@@ -24,20 +24,20 @@ inline constexpr int get_index(const index_t bi, const std::size_t counter, cons
 }
 
 template <typename index_t, std::size_t... dimension>
-inline constexpr index_t generate_index(const index_t bi, const std::size_t counter, integer_sequence<std::size_t,dimension...>)
+inline constexpr index_t generate_index(const index_t &bi, const std::size_t counter, integer_sequence<std::size_t,dimension...>)
 {
     return {get_index<index_t>(bi,counter,dimension)...};
 }
 
 template <typename index_t,
           typename integers = make_integer_sequence<std::size_t,std::tuple_size<index_t>::value>>
-inline constexpr index_t generate_index(const index_t bi, const std::size_t counter)
+inline constexpr index_t generate_index(const index_t &bi, const std::size_t counter)
 {
     return generate_index<index_t>(bi,counter,integers{});
 }
 
 template <typename list_t, typename index_t, std::size_t... counter>
-inline constexpr list_t generate_indices_helper(const index_t bi, integer_sequence<std::size_t,counter...>)
+inline constexpr list_t generate_indices_helper(const index_t &bi, integer_sequence<std::size_t,counter...>)
 {
     return {generate_index<index_t>(bi,counter)...};
 }
@@ -46,7 +46,7 @@ template <typename list_t,
           std::size_t Dim,
           typename index_t = std::array<int,Dim>,
           typename integers = make_integer_sequence<std::size_t,std::tuple_size<list_t>::value>>
-inline constexpr list_t generate_indices(const index_t bi)
+inline constexpr list_t generate_indices(const index_t &bi)
 {
     return generate_indices_helper<list_t,index_t>(bi,integers{});
 }

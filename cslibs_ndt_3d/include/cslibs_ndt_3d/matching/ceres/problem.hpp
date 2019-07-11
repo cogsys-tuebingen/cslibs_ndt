e8ed88c0a4fb1c260ca4b4678d2cad9cad03824c
Cornelia Schulz <cornelia.schulz@uni-tuebingen.de>
2019-07-11 16:03:56 +0200

    minor

--- cslibs_ndt_3d/include/cslibs_ndt_3d/matching/ceres/problem.hpp
+++ cslibs_ndt_3d/include/cslibs_ndt_3d/matching/ceres/problem.hpp
@@ -2,3 +2,7 @@
 
 #include <cslibs_ndt_3d/matching/ceres/map/gridmap_cost_functor.hpp>
 #include <cslibs_ndt_3d/matching/ceres/map/occupancy_gridmap_cost_functor.hpp>
+
+#include <ceres/problem.h>
+#include <ceres/solver.h>
+#include <ceres/covariance.h>
